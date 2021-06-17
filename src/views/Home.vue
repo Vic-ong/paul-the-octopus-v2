63b9076428c86c667e596ecca60852da696ce1d0
Vic Ong <vickyocc53@gmail.com>
2021-06-17 18:18:55 +0800

    feat(): Move about details down and add social links

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,55 +1,88 @@
 <template>
-  <div class="h-3/4 flex items-center justify-center">
-    <div class="flex flex-col items-center text-center">
-      <div class="relative heading text-primary">
-        Paul the Octopus
-        <div class="absolute -top-5 left-5">
-          <img
-            :src="logo"
-            class="h-7 animate-logo"
+  <div>
+    <div class="min-h-screen flex items-center justify-center">
+      <div class="flex flex-col justify-center items-center text-center space-y-24">
+        <div>
+          <div class="relative font-heading font-bold text-6xl text-primary">
+            Paul the Octopus
+            <div class="absolute -top-3 left-1">
+              <img
+                :src="logo"
+                class="h-7 animate-logo"
+              >
+            </div>
+          </div>
+
+          <div class="mt-7">
+            Calculates your character compatibility
+          </div>
+        </div>
+
+        <div>
+          <button
+            class="font-bold"
+            @click="router.push({ name: 'new-character' })"
           >
+            GET STARTED
+          </button>
+        </div>
+
+        <div>
+          <div>About this project</div>
+          <div
+            class="p-1 cursor-pointer flex items-center justify-center"
+            @click="scrollIntoNextView"
+          >
+            <i class="fi-sr-arrow-small-down text-2xl animate-bounce" />
+          </div>
         </div>
       </div>
+    </div>
 
-      <div class="mt-3">
-        Calculates your character compatibility
-      </div>
-
-      <div class="mt-8">
-        <button @click="router.push({ name: 'new-character' })">
-          Get Started
-        </button>
-      </div>
-
-      <hr class="border w-10 my-16">
-
-      <div class="max-w-5xl">
-        <div class="underline">
-          About
+    <div
+      id="about"
+      class="min-h-screen flex items-center justify-center"
+    >
+      <div class="flex flex-col items-center space-y-20">
+        <div class="max-w-3xl space-y-3">
+          <div class="heading text-center">
+            About
+          </div>
+          <div class="text-left">
+            This project is inspired by a project I worked on at Hawaii to research team-forming models based on individuals' skillset and personal goals.
+            Here, I made a simplified version of it to explore utilizations in game development.
+          </div>
         </div>
-        This project is inspired by a project I worked on at Hawaii to research team-forming models based on individuals' skillset and personal goals.
-        Here, I made a simplified version of it to explore utilizations in game development. The source code can be found
-        <a
-          href="https://github.com/Vic-ong/paul-the-octopus-v2"
-          target="_blank"
-        >
-          here
-        </a>.
-      </div>
 
-      <div class="mt-5 max-w-5xl">
-        <div class="underline">
-          Fun Fact
+        <div class="max-w-3xl space-y-3">
+          <div class="heading text-center">
+            Fun Fact
+          </div>
+          <div class="text-left">
+            The name "Paul The Octopus" is a tribute to an
+            <a
+              href="https://en.wikipedia.org/wiki/Paul_the_Octopus"
+              target="_blank"
+            >
+              actual octopus.
+            </a>
+            In the football/soccer domain, Paul has correctly predicted the winning teams in 12 out of 14 matches (85.7% accuracy)!
+            That's right folks, Paul may have been a brilliant analyst in disguise!
+          </div>
         </div>
-        The name "Paul The Octopus" is a tribute to an
-        <a
-          href="https://en.wikipedia.org/wiki/Paul_the_Octopus"
-          target="_blank"
-        >
-          actual octopus.
-        </a>
-        In the football/soccer domain, Paul has correctly predicted the winning teams in 12 out of 14 matches (85.7% accuracy)!
-        That's right folks, Paul may have been a brilliant analyst in disguise!
+
+        <hr class="border border-light w-60">
+
+        <div class="flex justify-center items-center space-x-14">
+          <a
+            v-for="icon in icons"
+            :key="icon.link"
+            :href="icon.link"
+            class="flex flex-col justify-center items-center w-12 h-12 bg-light rounded-full p-3 transition duration-500 ease-in-out transform hover:scale-125"
+          >
+            <component :is="icon.component" />
+          </a>
+        </div>
       </div>
     </div>
   </div>
@@ -59,15 +92,42 @@
   import { defineComponent } from 'vue';
   import { useRouter } from 'vue-router';
   import logo from '@/assets/logo.png';
+  import IconGithub from '@/components/Icons/IconGithub.vue';
+  import IconLinkedIn from '@/components/Icons/IconLinkedIn.vue';
+  import IconInstagram from '@/components/Icons/IconInstagram.vue';
 
   export default defineComponent({
     name: 'Home',
     setup() {
       const router = useRouter();
 
+      const icons = [
+        {
+          component: IconGithub,
+          link: 'https://github.com/Vic-ong/paul-the-octopus-v2',
+        },
+        {
+          component: IconLinkedIn,
+          link: 'https://www.linkedin.com/in/vickong/',
+        },
+        {
+          component: IconInstagram,
+          link: 'https://www.instagram.com/vic_ong/',
+        },
+      ];
+
+      const scrollIntoNextView = () => {
+        const el = document.getElementById('about');
+        if (el) {
+          el.scrollIntoView({ behavior: 'smooth' });
+        }
+      };
+
       return {
         router,
         logo,
+        icons,
+        scrollIntoNextView,
       };
     },
   });
@@ -76,10 +136,10 @@
 <style scoped>
 @keyframes rotate-logo {
   from {
-    transform: rotate(-5deg);
+    transform: rotate(3deg);
   }
   to {
-    transform: rotate(12deg);
+    transform: rotate(20deg);
   }
 }
 .animate-logo {
